598e188a037dd66b3ff826aafeabc4042f53c7f6
Deniz Cakmak <berfz23@gmail.com>
2020-07-22 13:49:35 +0300

    Add files via upload

---
+++ api/service.php
@@ -0,0 +1,50 @@
+<?php
+
+	$url = "127.0.0.1";
+	$database = "youdevodb";
+	$username = "root";
+	$password = "";			
+
+	$conn = mysqli_connect($url, $username, $password, $database, "3308");
+
+	if (!$conn) {
+		die("Connection failed: " .$conn->connect_error);
+	}
+
+	$method = $_SERVER['REQUEST_METHOD'];
+
+	if($method == "GET"){
+		$sql = "SELECT * FROM news";
+		$result = mysqli_query($conn, $sql);
+		$rows = array();
+
+			if (mysqli_num_rows($result) > 0) {
+				while ($r = mysqli_fetch_assoc($result)) {
+					array_push($rows, $r);
+		}
+		print json_encode($rows);
+	}
+	else{
+		echo "No data";
+		}
+	}
+
+	else if($method == "POST"){
+
+		
+		$title = $_POST['title'];
+		$content = $_POST['content'];
+		$category = $_POST['category'];
+		$photo_path = $_POST['photo_path'];
+
+		$sql_insert = "INSERT INTO news(title, content, category, photo_path) VALUES ('$title', '$content', '$category', '$photo_path')";
+
+		if (mysqli_query($conn, $sql_insert)){
+			echo "Item succesfully added to the database.";
+		}
+		else{
+			echo "ERROR: $sql_insert didn't run.".mysqli_error($conn);
+		}
+	}
+	mysqli_close($conn);
+?>
